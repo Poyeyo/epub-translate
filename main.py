3f5710d05048bef2a13fcb9b32f87ee93a2371f9
Poyeyo <poyeyo@arcadechaser.com>
2021-05-04 16:15:46 -0500

    Fix translations inside title tags

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ parser.add_argument("-s", "--sourcelang", dest="sourcelang", help="language of s
 parser.add_argument("-t", "--targetlang", dest="targetlang", help="language to translate to")
 parser.add_argument("-f", "--file", dest="filename", help="read epub from FILE", metavar="FILE")
 parser.add_argument("-o", "--outfile", dest="out_filename", help="write translated epub to FILE", metavar="FILE")
+parser.add_argument("-d", "--donotranslate", dest="donotranslate", help="test run, do not translate", action="store_true")
 
 args = parser.parse_args()
 
@@ -70,6 +71,8 @@ with ZipFile(filename, 'r') as zip:
 				file_data = ""
 				original_text = ""
 				translated_text = ""
+				tag = ""
+				tagspace = False
 
 				for char_index in range(len(originaldata)):
 					if (originaldata[char_index] == "<"):
@@ -79,32 +82,47 @@ with ZipFile(filename, 'r') as zip:
 							# Translate 
 							if args.verbose:
 								print("translatable:", original_text)
-							translation = translate_client.translate(str(original_text), source_language=sourcelang, target_language=targetlang)
-							translated_text = translation['translatedText']
+							# if tag in ["title", "style"]:
+							# 	print("tag in:", tag)
+							if args.donotranslate or tag in ["title", "style"]:
+								translated_text = original_text
+							else:
+								translation = translate_client.translate(str(original_text), source_language=sourcelang, target_language=targetlang)
+								translated_text = translation['translatedText']
 							if args.verbose:
 								print("translation:", translated_text)
 							if args.columns:
-								file_data += "<table style='width: 100%;'><tr><td style='width: 50%; padding-right:6pt; vertical-align: top;'>"
-								file_data += original_text
-								file_data += "</td><td style='width: 50%; padding-left:6pt; vertical-align: top;'>"
-								file_data += escape(translated_text)
-								file_data += "</tr></table>"
+								if tag in ["title", "style"]:
+									file_data += original_text
+								else:
+									file_data += "<table style=\"width: 100%;\"><tbody><tr><td style=\"width: 50%; padding-right:6pt; vertical-align: top;\">"
+									file_data += original_text
+									file_data += "</td><td style=\"width: 50%; padding-left:6pt; vertical-align: top;\">"
+									file_data += translated_text
+									file_data += "</td></tr></tbody></table>"
 							else:
-								file_data += escape(translated_text)
+								file_data += translated_text
 						else:
 							# Append to file_data
 							if args.verbose:
 								print("untranslatable:", original_text)
 							file_data += original_text
 						original_text = ""
+						tag = ""
+						tagspace = False
 					if (inside_tag < 1):
 						# print(originaldata[char_index], end='')
 						original_text += originaldata[char_index]
 					else:
 						# print(originaldata[char_index], end='')
 						file_data += originaldata[char_index]
+						if originaldata[char_index] == ' ':
+							tagspace = True
+						if not tagspace and originaldata[char_index] not in ['<', '>']:
+							tag += originaldata[char_index]
 					if (originaldata[char_index] == ">"):
 						inside_tag -= 1
+						# print("tag", tag)
 
 				zout.writestr(info.filename, file_data)
 
